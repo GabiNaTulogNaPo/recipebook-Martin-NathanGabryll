a0b5bc0b06dcecf357e4630bc3d18892ffa2d0f6
Nathan Martin <gabryllmartin@gmail.com>
2026-02-19 16:52:41 +0800

    edited views.py and urls.py to connect to database

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import recipe_list, recipe_1, recipe_2
+from .views import recipe_list, recipe_detail
 urlpatterns = [ 
     path('recipes/list/', recipe_list, name="recipe list"),
-    path('recipe/1/', recipe_1, name="recipe 1"),
-    path('recipe/2/', recipe_2, name="recipe 2"), 
+    path('recipe/<int:id>/', recipe_detail, name="recipe 1"),
 ]
 app_name = "ledger"
