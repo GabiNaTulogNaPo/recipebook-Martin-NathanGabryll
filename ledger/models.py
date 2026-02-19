8e0e88a0181a42c6d0fb08c452721f03955075eb
Nathan Martin <gabryllmartin@gmail.com>
2026-02-19 16:05:58 +0800

    Making functions for name and id for Recipe and Ingredient classes

--- ledger/models.py
+++ ledger/models.py
@@ -1,11 +1,20 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Ingredient(models.Model):
     name = models.CharField( max_length=50)
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse("ingredient_detail", args=[str(self.id)])
 
 class Recipe(models.Model):
     name = models.CharField( max_length=50)
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse("ingredient_detail", args=[str(self.id)])
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
