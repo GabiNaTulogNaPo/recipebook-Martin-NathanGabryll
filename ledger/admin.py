3d524fc4e3e8205f0ff88578b46e841008747bdb
Nathan Martin <gabryllmartin@gmail.com>
2026-03-02 12:03:18 +0800

    Creation UserAdmin and ProfileInLine

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,12 +1,30 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient, RecipeIngredient
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from .models import Recipe, Ingredient, RecipeIngredient, Profile
+
+
 # Register your models here.
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class UserAdmin(admin.BaseUserAdmin):
+    inlines = [ProfileInline,]
+
 
 class InlineRecipeIngredient(admin.TabularInline):
     model = RecipeIngredient
+
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
     inlines = [InlineRecipeIngredient]
 
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Ingredient)
 admin.site.register(Recipe, RecipeAdmin)
