5ac9a25667d6efa05b28d472b51ace8c91d910ec
Nathan Martin <gabryllmartin@gmail.com>
2026-03-12 16:06:55 +0800

    Added form for creating new recipe

--- ledger/views.py
+++ ledger/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+
 from .models import Recipe
+from .forms import RecipeForm
 
 def recipe_list(request):
     recipes = Recipe.objects.all()
@@ -12,7 +15,16 @@ def recipe_detail(request, id):
     context = {'recipe': Recipe.objects.get(id=id)}
     return render(request, "recipe_template.html", context)
 
-    
+@login_required
+def recipe_add(request):
+    form = RecipeForm()
+    if request.method == "POST":
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('ledger:recipe_list')
+    context = {"form": form}
+    return render(request, "recipe_add.html", context)
 
 
 
